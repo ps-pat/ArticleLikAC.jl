4e8bd4c08672984874d7cc6631126eccade10c1d
Patrick Fournier <pspat@beluga1.int.ets1.calculquebec.ca>
2023-12-02 10:23:50 -0500

    Shit should work.

--- scripts/study1.sh
+++ scripts/study1.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=study1
-#SBATCH --time=02:00:00
+#SBATCH --time=05:00:00
 #SBATCH --account=def-fabricel
-#SBATCH --ntasks=50
-#SBATCH --mem-per-cpu=1500M
+#SBATCH --ntasks=25
+#SBATCH --mem-per-cpu=2500M
 #SBATCH --mail-user=fournier.patrick@uqam.ca
 #SBATCH --mail-type=ALL
 
