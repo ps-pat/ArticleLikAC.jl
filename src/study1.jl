13347ccfb0afc7dd2bdbf9e04add509bfaaffded
Patrick Fournier <p_fournier@hushmail.com>
2023-12-04 15:27:48 -0500

    Test 100 tree for copula fitting by default.

--- src/study1.jl
+++ src/study1.jl
@@ -5,6 +5,6 @@ using Simulations
 MPI.Init()
 comm = MPI.COMM_WORLD
 
-study1(comm, n_is = 10000, f0 = 0.05, sample_prop = 5e-5, maf = 2.5e-1)
+study1(comm, n_is = 10000, f0 = 0.05, sample_prop = 5e-5, maf = 2.5e-1, M = 8, nα = 100)
 
 MPI.Finalize()
